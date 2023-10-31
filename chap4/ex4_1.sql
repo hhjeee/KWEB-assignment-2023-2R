ebe9bd676200751d027b986d4dc9f403bd5a6786
hhjeee <yoonhj0315@gmail.com>
2023-11-01 01:47:21 +0900

    ass2

---
+++ chap4/ex4_1.sql
@@ -0,0 +1,15 @@
+CREATE TABLE `students` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(10) NOT NULL,
+    `student_number` INT NOT NULL, -- 개별번호
+    `year` INT NOT NULL, -- 입학연도
+    `major` INT NOT NULL, -- 전공 
+    `phone_number` VARCHAR(11) NOT NULL,
+    `address` VARCHAR(50) NOT NULL,
+    `accumulated_credit` INT NOT NULL DEFAULT 0,
+    `average_grade` DOUBLE NOT NULL DEFAULT 0.0,
+    `is_in_school` TINYINT(1) NOT NULL DEFAULT 1,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- 학번 = 입학 연도 + 전공 + 개별 번호
---
+++ chap4/ex4_2.sql
@@ -0,0 +1,60 @@
+CREATE TABLE `users` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `password` VARCHAR(20) NOT NULL,
+    `nickname` VARCHAR(20) NOT NULL,
+    `img_link` VARCHAR(128) NOT NULL,
+    `profile_message` VARCHAR(128) NOT NULL,
+    `deleting_account` TINYINT(1) NOT NULL DEFAULT 0,
+    `date_join` DATETIME NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `channels` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(20) NOT NULL,
+    `user_create` INT NOT NULL, 
+    `channel_link` VARCHAR(128) NOT NULL,
+    `maximum_user` INT NOT NULL,
+    `deleting_account` TINYINT(1) NOT NULL DEFAULT 0,
+    `date_create` DATETIME NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`user_create`)
+    REFERENCES `users`(`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `chats` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `content` TEXT NOT NULL,
+    `user_write` INT NOT NULL, 
+    `channel` INT NOT NULL, 
+    `date_create_chats` DATETIME NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`user_write`)
+    REFERENCES `users`(`id`) ON DELETE CASCADE,
+    FOREIGN KEY (`channel`)
+    REFERENCES `channels`(`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `blocks` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `blocked` INT NOT NULL, 
+    `blocking` INT NOT NULL, 
+    `date_block` DATETIME NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`blocked`)
+    REFERENCES `users`(`id`) ON DELETE CASCADE,
+    FOREIGN KEY (`blocking`)
+    REFERENCES `users`(`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `follows` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+   `follower` INT NOT NULL, 
+   `followee` INT NOT NULL, 
+   `date_follow` DATETIME NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`follower`)
+    REFERENCES `users`(`id`) ON DELETE CASCADE,
+    FOREIGN KEY (`followee`)
+    REFERENCES `users`(`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
